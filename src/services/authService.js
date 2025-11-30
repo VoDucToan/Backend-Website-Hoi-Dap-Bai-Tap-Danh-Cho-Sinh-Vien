ed07f6feea87d7a32f12abbf1a5e5100312d7247
VoDucToan <hochut84@gmail.com>
2025-11-30 14:54:50 +0700

    deploy

--- src/services/authService.js
+++ src/services/authService.js
@@ -41,8 +41,8 @@ const handleCreateUser = async (display_name, email_address, password) => {
             // Send the vertification token to the user's email
             const transporter = nodemailer.createTransport({
                 host: "smtp.gmail.com",
-                port: 465,
-                secure: true,
+                port: 587,
+                secure: false,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS,
