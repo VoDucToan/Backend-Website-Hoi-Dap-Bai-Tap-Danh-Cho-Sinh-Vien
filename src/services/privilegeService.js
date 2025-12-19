8be4020a8f724b989c0978b058b3a59be1a402af
VoDucToan <hochut84@gmail.com>
2025-12-19 16:02:43 +0700

    deploy

--- src/services/privilegeService.js
+++ src/services/privilegeService.js
@@ -1,3 +1,4 @@
+const Privilege = require("../models/Privilege");
 const User = require("../models/User");
 const User_Privilege = require("../models/User_Privilege");
 const { handleNotifyForUser } = require("./notificationService");
@@ -8,130 +9,29 @@ const handleGrantPrivilegesForUser = async (idUser) => {
             where: {
                 id: idUser,
             },
+            attributes: ['reputation'],
         });
-        if (user.reputation >= 2) {
-            const upVotePostPrivilege = await User_Privilege.findOne({
-                where: {
-                    user_id: idUser,
-                    privilege_id: 1,
-                },
-            });
-            if (!upVotePostPrivilege) {
-                await User_Privilege.create({
-                    privilege_id: 1,
-                    user_id: idUser,
+
+        const privileges = await Privilege.findAll();
+        for (const privilege of privileges) {
+            if (user.reputation >= privilege.privilege_reputation) {
+                const userPrivilege = await User_Privilege.findOne({
+                    where: {
+                        user_id: idUser,
+                        privilege_id: privilege.id,
+                    },
                 });
-                const dataNotifyUser = await handleNotifyForUser(idUser, "Chức năng", "Bạn đã mở khóa chức năng bình chọn cho bình luận", "/")
-                if (dataNotifyUser?.EC !== 0) {
-                    return {
-                        EC: 2,
-                        EM: dataNotifyUser.EM,
-                    }
-                }
-            }
-        }
-        if (user.reputation >= 4) {
-            const upVotePostPrivilege = await User_Privilege.findOne({
-                where: {
-                    user_id: idUser,
-                    privilege_id: 2,
-                },
-            });
-            if (!upVotePostPrivilege) {
-                await User_Privilege.create({
-                    privilege_id: 2,
-                    user_id: idUser,
-                });
-                const dataNotifyUser = await handleNotifyForUser(idUser, "Chức năng", "Bạn đã mở khóa chức năng bình luận", "/")
-                if (dataNotifyUser?.EC !== 0) {
-                    return {
-                        EC: 2,
-                        EM: dataNotifyUser.EM,
-                    }
-                }
-            }
-        }
-        if (user.reputation >= 15) {
-            const upVotePostPrivilege = await User_Privilege.findOne({
-                where: {
-                    user_id: idUser,
-                    privilege_id: 3,
-                },
-            });
-            if (!upVotePostPrivilege) {
-                await User_Privilege.create({
-                    privilege_id: 3,
-                    user_id: idUser,
-                });
-                const dataNotifyUser = await handleNotifyForUser(idUser, "Chức năng", "Bạn đã mở khóa chức năng bình chọn lên cho bài viết", "/")
-                if (dataNotifyUser?.EC !== 0) {
-                    return {
-                        EC: 2,
-                        EM: dataNotifyUser.EM,
-                    }
-                }
-            }
-        }
-        if (user.reputation >= 30) {
-            const upVotePostPrivilege = await User_Privilege.findOne({
-                where: {
-                    user_id: idUser,
-                    privilege_id: 4,
-                },
-            });
-            if (!upVotePostPrivilege) {
-                await User_Privilege.create({
-                    privilege_id: 4,
-                    user_id: idUser,
-                });
-                const dataNotifyUser = await handleNotifyForUser(idUser, "Chức năng",
-                    "Bạn đã mở khóa chức năng bình chọn xuống cho bài viết", "/")
-                if (dataNotifyUser?.EC !== 0) {
-                    return {
-                        EC: 2,
-                        EM: dataNotifyUser.EM,
-                    }
-                }
-            }
-        }
-        if (user.reputation >= 75) {
-            const upVotePostPrivilege = await User_Privilege.findOne({
-                where: {
-                    user_id: idUser,
-                    privilege_id: 5,
-                },
-            });
-            if (!upVotePostPrivilege) {
-                await User_Privilege.create({
-                    privilege_id: 5,
-                    user_id: idUser,
-                });
-                const dataNotifyUser = await handleNotifyForUser(idUser, "Chức năng", "Bạn đã mở khóa chức năng thêm điểm thưởng", "/")
-                if (dataNotifyUser?.EC !== 0) {
-                    return {
-                        EC: 2,
-                        EM: dataNotifyUser.EM,
-                    }
-                }
-            }
-        }
-        if (user.reputation >= 150) {
-            const upVotePostPrivilege = await User_Privilege.findOne({
-                where: {
-                    user_id: idUser,
-                    privilege_id: 6,
-                },
-            });
-            if (!upVotePostPrivilege) {
-                await User_Privilege.create({
-                    privilege_id: 6,
-                    user_id: idUser,
-                });
-                const dataNotifyUser = await handleNotifyForUser(idUser, "Chức năng", "Bạn đã mở khóa chức năng tạo thẻ", "/")
-                if (dataNotifyUser?.EC !== 0) {
-                    return {
-                        EC: 2,
-                        EM: dataNotifyUser.EM,
+                if (!userPrivilege) {
+                    await User_Privilege.create({
+                        privilege_id: privilege.id,
+                        user_id: idUser,
+                    });
+                    const dataNotifyUser = await handleNotifyForUser(idUser, "Chức năng", `Bạn đã mở khóa chức năng ${privilege.privilege_type}`, "/", null);
+                    if (dataNotifyUser?.EC !== 0) {
+                        return {
+                            EC: 2,
+                            EM: dataNotifyUser.EM,
+                        }
                     }
                 }
             }
